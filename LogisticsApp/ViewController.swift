e1e9e6b1e34baaab39e26ae20f98a17cd9e3b130
Sandro <sandro.shedania.1@iliauni.edu.ge>
2025-01-15 06:21:49 -0800

    test

---
+++ LogisticsApp/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  LogisticsApp
+//
+//  Created by beqa on 15.01.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
